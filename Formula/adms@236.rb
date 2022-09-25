b7eb69d68af2a26d2f9922adc5f066d30a18307d
Omotade Iluromi <taderomi@outlook.com>
2022-09-25 14:34:21 +0100

    Initial commit of ADMS formula

---
+++ Formula/adms@236.rb
@@ -0,0 +1,36 @@
+class AdmsAT236 < Formula
+  desc "ADMS Code Generator for Verilog-AMS with Spice API"
+  homepage "https://sourceforge.net/projects/mot-adms/"
+  url "https://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.6.tar.gz/download"
+  sha256 "7307e8c63967696209c514545001fa496538112af7b812958950b22e0d45c9e6"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/adms[._-]v?(\d+(?:\.\d+)*)\.t}i)
+  end
+
+  head do
+    url "https://github.com/Qucs/ADMS.git", branch: "master"
+  
+    depends_on "bison" => :build
+  end
+
+  def install
+    
+    #system "echo $PWD"
+    system "./bootstrap.sh" if build.head?
+    
+    args = %W[
+      --prefix=#{prefix}
+      YACC=/opt/homebrew/opt/bison@3.8/bin/bison
+      BISON=/opt/homebrew/opt/bison@3.8/bin/bison
+    ]
+    
+    system "./configure", *args
+    system "make", "install"
+
+    # remove conflict lib files with libngspice
+    rm_rf Dir[lib/"adms"]
+  end
+
+end
