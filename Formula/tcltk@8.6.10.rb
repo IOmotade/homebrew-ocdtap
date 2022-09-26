7419c937d3da4e447269f7f045dc2eee1e239b64
IOmotade <IOmotade@users.noreply.github.com>
2022-09-26 22:00:11 +0100

    Update and rename tcltk@8.6.rb to tcltk@8.6.10.rb

--- Formula/tcltk@8.6.10.rb
+++ Formula/tcltk@8.6.10.rb
@@ -1,5 +1,5 @@
-class TcltkAT86 < Formula
-  desc "Tcl and Tk Version 8.6"
+class TcltkAT8610 < Formula
+  desc "Tcl and Tk Version 8.6.10"
   homepage "https://www.tcl.tk"
   url "https://prdownloads.sourceforge.net/tcl/tcl8.6.10-src.tar.gz"
   sha256 "5196dbf6638e3df8d5c87b5815c8c2b758496eb6f0e41446596c9a4e638d87ed"
@@ -33,8 +33,8 @@ class TcltkAT86 < Formula
 
     args = %W[
       --prefix=#{prefix}
-      --with-tcl=/opt/homebrew/Cellar/tcltk@8.6/8.6.10/lib
-      --with-tk=/opt/homebrew/Cellar/tcltk@8.6/8.6.10/lib
+      --with-tcl=/opt/homebrew/Cellar/tcltk@8.6.10/8.6.10/lib
+      --with-tk=/opt/homebrew/Cellar/tcltk@8.6.10/8.6.10/lib
       --x-includes=/opt/homebrew/include/X11
       --x-libraries=/opt/homebrew/lib/X11
     ]
@@ -47,7 +47,7 @@ class TcltkAT86 < Formula
       system "make"
       # system "sed -i '' -e 's,LIB_RUNTIME_DIR.*= $(libdir):/opt/X11/lib,LIB_RUNTIME_DIR = $(libdir),g' Makefile"
       system "make", "install"
-      system "install_name_tool /opt/homebrew/Cellar/tcltk@8.6/8.6.10/bin/wish8.6 -change /opt/homebrew/Cellar/tcltk@8.6/8.6.10/lib:/opt/homebrew/lib/X11/libtk8.6.dylib /opt/homebrew/Cellar/tcltk@8.6/8.6.10/lib/libtk8.6.dylib"
+      system "install_name_tool /opt/homebrew/Cellar/tcltk@8.6.10/8.6.10/bin/wish8.6 -change /opt/homebrew/Cellar/tcltk@8.6.10/8.6.10/lib:/opt/homebrew/lib/X11/libtk8.6.dylib /opt/homebrew/Cellar/tcltk@8.6.10/8.6.10/lib/libtk8.6.dylib"
     }
 
   end
