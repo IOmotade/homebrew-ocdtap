6d31b76221f95870e5aa7cf3a7b3e81e0c8a51e2
Omotade Iluromi <taderomi@outlook.com>
2022-09-26 20:04:12 +0100

    Added netgen formula

---
+++ Formula/netgen@15.rb
@@ -0,0 +1,38 @@
+class NetgenAT15 < Formula
+  desc "Netgen v1.5 Netlist Comparison(LVS) and Format Manipulation"
+  homepage "http://opencircuitdesign.com/netgen/"
+  url "http://opencircuitdesign.com/netgen/archive/netgen-1.5.233.tgz"
+  sha256 "31d0d964892810b63b7450cc6614c64e8b4b7c681ddce1393692ccf39ae760ba"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/netgen[._-]v?(\d+(?:\.\d+)*)\.t}i)
+  end
+
+  head do
+    url "git://opencircuitdesign.com/netgen", branch: "netgen-1.5"
+  end
+
+  def install
+        
+    args = %W[
+      --prefix=#{prefix}
+      --with-tcl=/opt/homebrew/Cellar/tcltk@8.6/8.6.10/lib
+      --with-tk=/opt/homebrew/Cellar/tcltk@8.6/8.6.10/lib
+    ]
+    
+    system "./configure", *args
+    system "make", "install"
+    
+    system "/opt/homebrew/bin/wget", "https://github.com/IOmotade/homebrew-ocdtap/raw/master/Fixes/tclnetgen.dylib"
+
+    args = %W[
+      #{prefix}/lib/netgen/tcl/
+    ]
+
+    system "chmod +x tclnetgen.dylib"
+    FileUtils.move "tclnetgen.dylib", *args
+
+  end
+
+end
