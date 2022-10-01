e784e38baa3179e26be38f05b0ca95af7b057c4c
Omotade Iluromi <taderomi@outlook.com>
2022-10-01 14:28:26 +0100

    Updated open-pdks condarc-yaml-error-patch

--- Patches/open-pdks@1.0.340/condarc-yaml-error-patch.py
+++ Patches/open-pdks@1.0.340/condarc-yaml-error-patch.py
@@ -1,13 +1,17 @@
 import sys
 
+# Get file's absolute path
 buildpath = sys.argv[1]
 filepath = f"{buildpath}/sources/skywater-pdk/third_party/make-env/conda.mk"
 
+# Read file
 with open(filepath, 'r') as file:
   contents = file.readlines()
   
-contents[120-1] = "$(CONDA_SETUP_PATCH): $(RM) $(CONDA_DIR)/.condarc\n"
-contents[144-1] = "$(CONDA_ENV_PYTHON): $(ENVIRONMENT_FILE) $(REQUIREMENTS_FILE) | $(CONDA_PYTHON) $(CONDA_SETUP_PATCH) $(CONDA_PKGS_DEP) $(CONDA_ENVS_DIR) $(CONDA_PYVENV)\n"
+# Edit contents
+lineIdx = 145-1
+contents[lineIdx] = f"\t$(RM) $(CONDA_DIR)/.condarc\n{contents[lineIdx]}\n"
 
+# Write file
 with open(filepath, 'w') as file:
   file.write("".join(contents))
